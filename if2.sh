5e32120aac89b22a3b87aeec6ee72ce27a55bca5
tmzkxm1 <tmzkxm1@naver.com>
2026-07-22 04:28:09 +0000

    if문 테스트 스크립트 2, 3 추가

---
+++ if2.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+ABC=123
+BCD=123
+
+if [ $ABC -eq 121 ]; then
+        echo "equal to"
+else
+        echo "not equal to"
+fi
---
+++ if3.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+ABC=123
+BCD=123
+	if [ $ABC -gt 124 ]; then
+		echo "true"
+else
+		echo "false"
+fi
